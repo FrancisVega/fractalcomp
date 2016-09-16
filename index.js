d03dfc9a2cc1bde9782243d06dffd2e680ba0729
Francis <hisconer@gmail.com>
2016-09-16 10:59:16 +0200

    yaml and handlebars added

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ var colors = require('colors')
 var R = require('ramda')
 
 /**
- * Escribe datos (string) en un archivo y si existe da un error
- * @parms f {string} El path del archivo a escribir
- * @parms content {string} El contenido que queremos escribir
+ * Write data into file
+ * @parms f {string} File path
+ * @parms content {string} File conent
  */
 const writeFile = (f, content) => {
   if(!isFile(f)){
     fs.writeFileSync(f, content)
   } else {
-    console.log(`El archivo ${f} ya existe. Better call Saul.`.red);
+    console.log(`The file ${f} already exists. Better call Saul.`.red);
   }
 }
 
 /**
- * Comprueba si existe el archivo f
+ * Check if file exists
  */
 const isFile = f => {
   try {
@@ -33,9 +33,9 @@ const isFile = f => {
 }
 
 /**
- * Obtiene el contenido de un template reemplazando @@name por name
- * @parms ftype {string} El tipo de fichero que quieres obtener
- * @parms name {string} El texto que quieres reemplazar por @@name
+ * Get a template file replacing @@name with name
+ * @parms ftype {string} File type
+ * @parms name {string} Content of @@name
  * @return {string}
  */
 const contentOfFile = (ftype, name) => {
@@ -52,24 +52,21 @@ const contentOfFile = (ftype, name) => {
     .replace(/@@name/g, name)
 }
 
-/*
- * Objeto base con el contenido del CLI
- */
 let comp = {
   "type": "nunjucks",
+  "path": "",
   "name": "newcomponent",
+  "fullPath": "",
   "config": false,
   "readme": false,
   "css": false
 }
 
-// Commander Parser
 let app = require('commander')
 app
   .version('1.0')
   .option('-a, --all', 'make component with all files (Default formats)')
-  .option('-n, --nunjucks', 'nunjucks engine')
-  .option('-b, --handlebars', 'handlerbars engine')
+  .option('-t --type <type>', 'Component Engine', /^(nunjucks|handlebars)$/i, 'nunjucks')
   .option('-r, --readme', 'readme.md file')
   .option('-c, --css', 'css file')
   .option('-y, --yaml', 'yaml file')
@@ -78,84 +75,101 @@ app
   .option('-v, --verbose', 'Verbose mode')
   .parse(process.argv)
 
-// Si no especificamos un nombre, lo tomamos de la carpeta actual
+// If there is no name use the current folder instead.
 if(!app.args[0]) {
   comp.name = path.basename(process.cwd())
     .split(/\d+[-_ ]+/)
     .join("")
 } else {
-  comp.name = app.args[0]
+  comp.dir = path.dirname(app.args[0])
+  comp.name = path.basename(app.args[0])
+  comp.fullPath = app.args[0]
 }
 
-// Parseamos el nombre humanizándolo
+// A simple humanize.
+// TODO: Refactor with ramda please :P
 comp.name =
   comp.name.toLowerCase()
   .split(/[-_ ]/g)
-    .map((s, x) => {
-      if (x>0) {
-        return `${s.charAt(0).toUpperCase()}${s.slice(1)}`
-      } else {
-        return s.toLowerCase();
-      }
-    })
-    .join("")
+  .map((s, x) => {
+    if (x>0) {
+      return `${s.charAt(0).toUpperCase()}${s.slice(1)}`
+    } else {
+      return s.toLowerCase();
+    }
+  })
+  .join("")
 
-  if (app.all) {
-    comp.type = "nunjucks"
-    comp.config = "javascript"
-    comp.css = true
-    comp.readme = true
-  } else {
-    // Component Type (Engine)
-    if (app.handlebars) comp.type = 'handlebars'
-    if (app.nunjucks) comp.type = 'nunjucks'
 
-    // CSS
-    if (app.css) comp.css = true
+// --all
+if (app.all) {
+  comp.type = "nunjucks"
+  comp.config = "javascript"
+  comp.css = true
+  comp.readme = true
+} else {
+  // Engine
+  comp.type = app.type
 
-    // Config File And Format
-    if (app.yaml) comp.config = 'yaml'
-    if (app.javascript) comp.config = 'javascript'
-    if (app.json) comp.config = 'json'
+  // CSS
+  if (app.css) comp.css = true
 
-    // README.md
-    if (app.readme) comp.readme = true
-  }
+  // Config File And Format
+  if (app.yaml) comp.config = 'yaml'
+  if (app.javascript) comp.config = 'javascript'
+  if (app.json) comp.config = 'json'
 
-  if(app.verbose) {
-    console.log("")
-    console.log("  New Fractal Component".yellow)
-    console.log("")
-    console.log('  Name:      '.gray + comp.name)
-    console.log('  Type:      '.gray + comp.type)
-    console.log('  Config:    '.gray + comp.config)
-    console.log('  CSS:       '.gray + comp.css)
-    console.log('  README.md: '.gray + comp.readme)
-    console.log("")
-  }
+  // README.md
+  if (app.readme) comp.readme = true
+}
 
-  const cwd = process.cwd()
+if(app.verbose) {
+  console.log("")
+  console.log("  New Fractal Component".yellow)
+  console.log("")
+  console.log('  Name:      '.gray + comp.name)
+  console.log('  Type:      '.gray + comp.type)
+  console.log('  Config:    '.gray + comp.config)
+  console.log('  CSS:       '.gray + comp.css)
+  console.log('  README.md: '.gray + comp.readme)
+  console.log("")
+}
 
-  // comp
-  if (comp.type === "nunjucks") {
-    let f = `${cwd}/${comp.name}.njk`
-    writeFile(f, contentOfFile("nunjucks", comp.name))
-  }
+// Make component dir if needed
+try {
+  fs.mkdirs(comp.dir)
+} catch (e) {}
 
-  // config
-  if (comp.config === "javascript")
-    writeFile(`${cwd}/${comp.name}.config.js`, contentOfFile("config_js", comp.name))
-  if (comp.config === "json")
-    writeFile(`${cwd}/${comp.name}.config.json`, contentOfFile("config_json", comp.name))
-  if (comp.config === "yaml")
-    writeFile(`${cwd}/${comp.name}.config.yaml`, contentOfFile("config_yaml", comp.name))
 
-  // css
-  if (comp.css)
-    writeFile(`${cwd}/${comp.name}.css`, contentOfFile("css", comp.name))
+// comp
+switch (comp.type) {
+  case 'nunjucks':
+    writeFile(`${comp.fullPath}.njk`, contentOfFile("nunjucks", comp.name))
+    break;
+  case 'handlebars':
+    writeFile(`${comp.fullPath}.hbs`, contentOfFile("handlebars", comp.name))
+    break;
+}
 
-  // readme
-  if (comp.readme)
-    writeFile(`${cwd}/README.md`, contentOfFile("readme", comp.name))
+// config
+switch (comp.config) {
+  case 'javascript':
+    writeFile(`${comp.fullPath}.config.js`, contentOfFile("config_js", comp.name))
+    break;
+  case 'json':
+    writeFile(`${comp.fullPath}.config.json`, contentOfFile("config_json", comp.name))
+    break;
+  case 'yaml':
+    writeFile(`${comp.fullPath}.config.yaml`, contentOfFile("config_yaml", comp.name))
+    break;
+}
 
-  process.exit(0)
+// css
+if (comp.css)
+  writeFile(`${comp.fullPath}.css`, contentOfFile("css", comp.name))
+
+// readme
+if (comp.readme)
+  writeFile(`${comp.dir}/README.md`, contentOfFile("readme", comp.name))
+
+process.exit(0)
