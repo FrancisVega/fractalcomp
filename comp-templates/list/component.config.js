6d3ada7906e2121304e06f92e605aaf78ca8cca9
Francis <hisconer@gmail.com>
2016-11-02 23:12:18 +0100

    Add template list

---
+++ comp-templates/list/component.config.js
@@ -0,0 +1,14 @@
+/* @@name */
+module.exports = {
+  "name": "@@name",
+  "context": {
+    "modifiers": [],
+    "mix": "",
+    "items": [
+      {"name": "Name of item one", "href": "href-of-item-one"},
+      {"name": "Name of item two", "href": "href-of-item-two"},
+      {"name": "Name of item three", "href": "href-of-item-three"},
+      {"name": "Name of item four", "href": "href-of-item-four"}
+    ]
+  }
+}
