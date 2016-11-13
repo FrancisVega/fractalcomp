e3a85a05a1e9446d13e3f742e1ba9f6caca006a1
Francis <hisconer@gmail.com>
2016-11-13 10:45:27 +0100

    Add generic comments

--- comp-templates/base/component.config.js
+++ comp-templates/base/component.config.js
@@ -1,8 +1,32 @@
+/*
+
+Esto es un archivo de configuración genérico creado por el comando newcomp.
+
+Modificadores BEM:
+"mods": []
+
+Clases adicionales:
+"classes": []
+
+Clase BEM-MIX:
+"mix": ""
+
+> Si no quieres este comentario llama a newcomp con el flag --nc
+  newcomp --nc --all button
+
+*/
+
 /* @@name */
+
 module.exports = {
+
   "name": "@@name",
+  "preview": "@dark",
+
   "context": {
-    "modifiers": [],
+    "mods": [],
+    "classes": [],
     "mix": ""
   }
+
 }
