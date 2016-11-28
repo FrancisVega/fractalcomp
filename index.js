95525b7061ae332ea6aa169e3aefca1088461a2e
Francis <hisconer@gmail.com>
2016-11-28 23:29:33 +0100

    small refactor

--- index.js
+++ index.js
@@ -9,27 +9,29 @@ const colors = require('colors')
 const camelCase = require('camelcase')
 const util = require('./util.js')
 
+// Constants
+const compBaseName = 'component'
+const compConfigBaseName = '.config'
+const extensions =
+  { "handlebars": '.hbs'
+  , "nunjucks": '.nun'
+  , "javascript": '.js'
+  , "json": '.json'
+  , "yaml": '.yaml'
+  , "css": '.css'
+  , "scss": '.scss'
+  , "markdown": '.md' }
+
+
 /**
  * Get a template file replacing @@name with name
  * @parms ftype {string} File type
  * @parms name {string} Content of @@name
  * @return {string}
  */
-const getTemplate = (ctype, ftype, name) => {
-  const FTYPES = {
-    "nunjucks": "component.nun",
-    "handlebars": "component.hbs",
-    "css": "component.css",
-    "scss": "component.scss",
-    "readme": "README.md",
-    "config_yaml": "component.config.yaml",
-    "config_json": "component.config.json",
-    "config_js": "component.config.js"
-  }
-  return fs.readFileSync(
-    `${__dirname}/comp-templates/${ctype}/${FTYPES[ftype]}`, 'utf8')
-    .replace(/@@name/g, name)
-}
+const getTemplate = (ctype, ftype, name) =>
+  fs.readFileSync ( `${__dirname}/comp-templates/${ctype}/${ftype}` , 'utf8' )
+    .replace( /@@name/g, name )
 
 const app = require('commander')
 app
@@ -47,43 +49,40 @@ app
   .parse(process.argv)
 
 let comp = {
-  "template": "base",
-  "type": "nunjucks",
-  "path": "",
-  "name": "newcomponent",
-  "fullPath": "",
-  "config": false,
-  "readme": false,
-  "styles": "scss"
+  'template': 'base',
+  'type': 'nunjucks',
+  'path': '',
+  'name': 'newcomponent',
+  'fullPath': '',
+  'config': false,
+  'readme': false,
+  'styles': 'scss'
 }
 
 // If there is no name use the current folder instead.
 if(!app.args[0]) {
   comp.name = path.basename(process.cwd())
     .split(/\d+[-_ ]+/)
-    .join("")
-  comp.dir = "."
+    .join('')
+  comp.dir = '.'
   comp.fullPath = `${comp.dir}/${comp.name}`
 } else {
   // if -d create a directory with the component name
   comp.name = path.basename(app.args[0])
   comp.fullPath = app.args[0]
   if (app.directory) {
-    comp.dir = path.dirname(app.args[0]) + "/" + comp.name
+    comp.dir = path.dirname(app.args[0]) + '/' + comp.name
   } else {
     comp.dir = path.dirname(app.args[0])
   }
 }
 
-// camelcase the comp name
-//comp.name = camelCase(comp.name)
-
 // --all
 if (app.all) {
-  comp.type = "nunjucks"
-  comp.config = "javascript"
+  comp.type = 'nunjucks'
+  comp.config = 'javascript'
   comp.readme = true
-  comp.styles = "css"
+  comp.styles = 'css'
 } else {
   // Engine
   comp.type = app.type
@@ -108,52 +107,117 @@ try { fs.mkdirsSync(comp.dir) } catch (e) {}
 
 // comp
 switch (comp.type) {
+
   case 'nunjucks':
-    util.writeFile(`${comp.dir}/${comp.name}.nun`, getTemplate(comp.template, "nunjucks", comp.name))
+    util.writeFile
+      ( `${comp.dir}/${comp.name}${extensions.nunjucks}`
+      , getTemplate
+        ( comp.template
+        , `${compBaseName}${extensions.nunjucks}`
+        , comp.name
+        )
+      )
     break
+
   case 'handlebars':
-    util.writeFile(`${comp.dir}/${comp.name}.hbs`, getTemplate(comp.template, "handlebars", comp.name))
+    util.writeFile
+      ( `${comp.dir}/${comp.name}${extensions.handlebars}`
+      , getTemplate
+        ( comp.template
+        , `${compBaseName}${extensions.handlebars}`
+        , comp.name
+        )
+      )
     break
+
 }
 
 // config
 switch (comp.config) {
+
   case 'javascript':
-    util.writeFile(`${comp.dir}/${comp.name}.config.js`, getTemplate(comp.template, "config_js", comp.name))
+    util.writeFile
+      ( `${comp.dir}/${comp.name}${compConfigBaseName}${extensions.javascript}`
+      , getTemplate
+        ( comp.template
+        , `${compBaseName}${compConfigBaseName}${extensions.javascript}`
+        , comp.name
+        )
+      )
     break
+
   case 'json':
-    util.writeFile(`${comp.dir}/${comp.name}.config.json`, getTemplate(comp.template, "config_json", comp.name))
+    util.writeFile
+      ( `${comp.dir}/${comp.name}${compConfigBaseName}${extensions.json}`
+      , getTemplate
+        ( comp.template
+        , `${compBaseName}${compConfigBaseName}${extensions.json}`
+        , comp.name
+        )
+      )
     break
+
   case 'yaml':
-    util.writeFile(`${comp.dir}/${comp.name}.config.yaml`, getTemplate(comp.template, "config_yaml", comp.name))
+    util.writeFile
+      ( `${comp.dir}/${comp.name}${compConfigBaseName}${extensions.yaml}`
+      , getTemplate
+        ( comp.template
+        , `${compBaseName}${compConfigBaseName}${extensions.yaml}`
+        , comp.name
+        )
+      )
     break
 }
 
 // styles
 switch (comp.styles) {
+
   case 'scss':
-    util.writeFile(`${comp.dir}/_${comp.name}.scss`, getTemplate(comp.template, "scss", camelCase(comp.name)))
+    util.writeFile
+      ( `${comp.dir}/_${comp.name}${extensions.scss}`
+      , getTemplate
+        ( comp.template
+        , `${compBaseName}${extensions.scss}`
+        , comp.name
+        )
+      )
     break
+
   case 'css':
-    util.writeFile(`${comp.dir}/${comp.name}.css`, getTemplate(comp.template, "css", camelCase(comp.name)))
+    util.writeFile
+      ( `${comp.dir}/${comp.name}${extensions.css}`
+      , getTemplate
+        ( comp.template
+        , `${compBaseName}${extensions.css}`
+        , comp.name
+        )
+      )
     break
+
 }
 
 // readme
 if (comp.readme)
-  util.writeFile(`${comp.dir}/README.md`, getTemplate(comp.template, "readme", comp.name))
+  util.writeFile
+    ( `${comp.dir}/README.md`
+    , getTemplate
+      ( comp.template
+      , `README${extensions.markdown}`
+      , comp.name
+      )
+    )
 
 if(app.verbose) {
-  console.log("")
-  console.log("  New Fractal Component".yellow)
-  console.log("")
+  console.log('')
+  console.log('  New Fractal Component'.yellow)
+  console.log('')
   console.log('  Template:  '.gray + comp.template)
   console.log('  Name:      '.gray + comp.name)
   console.log('  Type:      '.gray + comp.type)
   console.log('  Config:    '.gray + comp.config)
   console.log('  Styles:    '.gray + comp.styles)
   console.log('  README.md: '.gray + comp.readme)
-  console.log("")
+  console.log('')
 }
 
 process.exit(0)
